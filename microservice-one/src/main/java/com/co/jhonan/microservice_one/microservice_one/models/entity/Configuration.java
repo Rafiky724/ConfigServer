8ed1123d4e22f6a351b9030a9fbeb6323ecf3a1e
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2024-05-29 15:03:45 -0500

    eureka rename

--- microservice-one/src/main/java/com/co/jhonan/microservice_one/microservice_one/models/entity/Configuration.java
+++ microservice-one/src/main/java/com/co/jhonan/microservice_one/microservice_one/models/entity/Configuration.java
@@ -2,7 +2,7 @@ package com.co.jhonan.microservice_one.microservice_one.models.entity;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties("microservice-one")
+@ConfigurationProperties("eureka")
 @org.springframework.context.annotation.Configuration
 public class Configuration {
 	private String value;
